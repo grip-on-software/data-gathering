5b2f0ccb3354304a4f4217f1b8c76ff642c5dcef
Thomas Prikkel <thomasprikkel@Thomass-MacBook-Air.local>
2016-11-09 14:17:07 +0100

    File restructuring

--- retrieveproject.sh
+++ retrieveproject.sh
@@ -12,6 +12,9 @@ echo "Creation of issue JSON data is done"
 echo "Initialising python script for retrieving git data"
 python git_to_json.py $project_id $git_project_id
 echo "Creation of git JSON data is done"
+
+##########################################
 echo "Initialising java script for storage"
+
 
 
