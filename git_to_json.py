45d67d81dc82573e4f5f2c8d38a7f54ed3be849d
Thomas Prikkel <thomasprikkel@Thomass-MacBook-Air.local>
2016-12-05 09:58:52 +0100

    Git change

--- git_to_json.py
+++ git_to_json.py
@@ -4,6 +4,7 @@ import json
 import datetime
 import pprint
 import sys
+import os
 
 project_repo_folder = "REPO"
 project_name = "PROJ1"
@@ -20,76 +21,88 @@ except IndexError:
 print project_name
 print project_repo_folder
 
-json_data=open(project_name+"/data_sprint.json").read()
-sprint_data = json.loads(json_data)
-sprint_git_data= {}
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
 
-for sprint in sprint_data:
-	sprint_git_data[int(sprint['id'])] = {}
-	sprint_git_data[int(sprint['id'])]['start_date'] = datetime.datetime.strptime(sprint['start_date'], '%Y-%m-%d %H:%M:%S')
-	sprint_git_data[int(sprint['id'])]['end_date'] = datetime.datetime.strptime(sprint['end_date'], '%Y-%m-%d %H:%M:%S')
 
-repo = Repo("../Project-git-repos/" + project_repo_folder)
-o = repo.remotes.origin
-o.pull()
+def get_git_data(git_commit_data, project_name, repo_name):
+	json_data=open(project_name+"/data_sprint.json").read()
+	sprint_data = json.loads(json_data)
+	sprint_git_data= {}
 
-skip = 0
-iterate_size = 1000
-iterate_max = 10000000
+	for sprint in sprint_data:
+		sprint_git_data[int(sprint['id'])] = {}
+		sprint_git_data[int(sprint['id'])]['start_date'] = datetime.datetime.strptime(sprint['start_date'], '%Y-%m-%d %H:%M:%S')
+		sprint_git_data[int(sprint['id'])]['end_date'] = datetime.datetime.strptime(sprint['end_date'], '%Y-%m-%d %H:%M:%S')
 
-count = 1
+	repo = Repo("project-git-repos/" + project_name + "/" + repo_name)
+	o = repo.remotes.origin
+	o.pull()
 
-git_commit_data = []
-
-commits = repo.iter_commits('master', max_count=iterate_size, skip=skip)
-
-while commits and iterate_size + skip <= iterate_max:
-	if iterate_size is 0:
-		break
-
-	stop_iteration = True
-
-	for commit in commits:
-		stop_iteration = False
-		git_commit = {}
-		git_commit['commit_id'] = str(commit.hexsha)
-		git_commit['sprint_id'] = str(0)
-
-		cs = commit.stats
-		cstotal = cs.total
-
-		git_commit['insertions'] = str(cstotal['insertions'])
-		git_commit['deletions'] = str(cstotal['deletions'])
-		git_commit['number_of_files'] = str(cstotal['files'])
-		git_commit['number_of_lines'] = str(cstotal['lines'])
-
-		git_commit['message'] = str(commit.message.encode('utf-8'))
-		git_commit['size_of_commit'] = str(commit.size)
-		git_commit['type'] = str(commit.type)
-		git_commit['developer'] = str(commit.author.name)
-		git_commit['developer_email'] = str(commit.author.email)
-
-		commit_datetime = datetime.datetime.fromtimestamp(commit.committed_date)
-		git_commit['commit_date'] = datetime.datetime.strftime(commit_datetime, '%Y-%m-%d %H:%M:%S')
-
-		for sprint_id in sprint_git_data:
-			sprint = sprint_git_data[sprint_id]
-			if commit_datetime >= sprint['start_date'] and commit_datetime <= sprint['end_date']:
-				git_commit['sprint_id'] = str(sprint_id)
-				break
-		git_commit_data.append(git_commit)
-
-	print 'Analysed commits up to ',
-	print iterate_size+skip
-
-	skip = skip + iterate_size
-	if skip + iterate_size > iterate_max:
-		iterate_size = iterate_max - skip
+	skip = 0
+	iterate_size = 1000
+	iterate_max = 10000000
 
 	commits = repo.iter_commits('master', max_count=iterate_size, skip=skip)
-	#stop iteration if no commits are found in previous round
-	if stop_iteration:
-		commits = False
+
+	while commits and iterate_size + skip <= iterate_max:
+		if iterate_size is 0:
+			break
+
+		stop_iteration = True
+
+		for commit in commits:
+			stop_iteration = False
+			git_commit = {}
+			git_commit['git_repo'] = str(repo_name)
+			git_commit['commit_id'] = str(commit.hexsha)
+			git_commit['sprint_id'] = str(0)
+
+			cs = commit.stats
+			cstotal = cs.total
+
+			git_commit['insertions'] = str(cstotal['insertions'])
+			git_commit['deletions'] = str(cstotal['deletions'])
+			git_commit['number_of_files'] = str(cstotal['files'])
+			git_commit['number_of_lines'] = str(cstotal['lines'])
+
+			git_commit['message'] = str(commit.message.encode('utf-8'))
+			git_commit['size_of_commit'] = str(commit.size)
+			git_commit['type'] = str(commit.type)
+			git_commit['developer'] = str(commit.author.name)
+			git_commit['developer_email'] = str(commit.author.email)
+
+			commit_datetime = datetime.datetime.fromtimestamp(commit.committed_date)
+			git_commit['commit_date'] = datetime.datetime.strftime(commit_datetime, '%Y-%m-%d %H:%M:%S')
+
+			for sprint_id in sprint_git_data:
+				sprint = sprint_git_data[sprint_id]
+				if commit_datetime >= sprint['start_date'] and commit_datetime <= sprint['end_date']:
+					git_commit['sprint_id'] = str(sprint_id)
+					break
+			git_commit_data.append(git_commit)
+
+		print 'Analysed commits up to ',
+		print iterate_size+skip
+
+		skip = skip + iterate_size
+		if skip + iterate_size > iterate_max:
+			iterate_size = iterate_max - skip
+
+		commits = repo.iter_commits('master', max_count=iterate_size, skip=skip)
+		#stop iteration if no commits are found in previous round
+		if stop_iteration:
+			commits = False
+
+		return git_commit_data
+
+
+git_commit_data = []
+repos = get_immediate_subdirectories("project-git-repos/" + project_name)
+for repo_name in repos:
+	print repo_name
+	git_commit_data = get_git_data(git_commit_data, project_name, repo_name)
 
 #START dump data
 with open(data_folder+'/data_commits.json', 'w') as outfile:
