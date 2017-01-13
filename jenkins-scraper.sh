d894f15b4a39b79ec317f43236e01fe85b83c660
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-01-13 09:59:31 +0100

    New scraper and importer
    The scraper now has less duplicated code for each project and handles
    errors by reverting the tracking files in the workspace.

--- jenkins-scraper.sh
+++ jenkins-scraper.sh
@@ -1,4 +1,58 @@
 #!/bin/bash
+
+ROOT=$PWD
+
+# Declare list of projects to scrape from
+listOfProjects="PROJ1 PROJ2 PROJN"
+
+# Files that are backed up in case of errors for each project
+restoreFiles="jira-updated.txt git-commit.json history_line_count.txt"
+
+function error_handler() {
+	echo "Reverting workspace tracking data..."
+	for project in $listOfProjects
+	do
+		for restoreFile in $restoreFiles
+		do
+			if [ -e "$ROOT/$project/$restoreFile.bak" ]; then
+				mv "$ROOT/$project/$restoreFile.bak" "$ROOT/$project/$restoreFile"
+			else
+				rm -f "$ROOT/$project/$restoreFile"
+			fi
+		done
+	done
+}
+
+function status_handler() {
+	set +e
+	"$@"
+	local status=$?
+	set -e
+	if [ $status -ne 0 ]; then
+		echo "[$@] Failed with status code $status" >&2
+		error_handler
+	fi
+	return $status
+}
+
+function update_repositories() {
+	url=$1
+	shift
+	listOfRepos=$*
+	for repo in $listOfRepos
+	do
+		# look for empty dir
+		if [ "$(ls -A $repo 2>/dev/null)" ]; then
+			echo "$repo is not Empty"
+			cd "$repo"
+			git pull $(printf $url $repo)
+			cd ..
+		else
+			git clone $(printf $url $repo)
+		fi
+	done
+}
+
 pip install jira
 pip install gitpython
 pip install requests
@@ -9,90 +63,41 @@ cp scripts/history_to_json.py history_to_json.py
 cp scripts/importerjson.jar importerjson.jar
 cp -r scripts/lib lib
 
-## declare an array variable
-listOfProjects="PROJ1 PROJ2 PROJN"
-
-## now loop through the above array
+## now loop through the list of projects
 for project in $listOfProjects
 do
-   echo "$project"
-   mkdir -p project-git-repos/$project
-   cd project-git-repos/$project
-   if [ $project = "PROJ2" ]; then
-      listOfRepos="REPO1 REPO2 REPON"
-      for repo in $listOfRepos
-      do
-         # look for empty dir
-         if [ "$(ls -A $repo)" ]; then
-            echo "$repo is not Empty"
-            cd "$repo"
-            git pull http://GITLAB_SERVER.localhost/project/"$repo".git
-            cd ..
-         else
-            git clone http://GITLAB_SERVER.localhost/project/"$repo".git
-         fi
-      done
-   elif [ $project = "PROJ1" ]; then
-      listOfRepos="REPO1 REPO2 REPON"
-      for repo in $listOfRepos
-      do
-         # look for empty dir
-         if [ "$(ls -A $repo)" ]; then
-            echo "$repo is not Empty"
-            cd "$repo"
-            git pull http://GITLAB_SERVER.localhost/project/"$repo"
-            cd ..
-         else
-            git clone http://GITLAB_SERVER.localhost/project/"$repo"
-         fi
-      done
-   elif [ $project = "PROJ4" ]; then
-      listOfRepos="REPO1 REPO2 REPON"
-      for repo in $listOfRepos
-      do
-         # look for empty dir
-         if [ "$(ls -A $repo)" ]; then
-            echo "$repo is not Empty"
-            cd "$repo"
-            git pull http://GITLAB_SERVER.localhost/project/"$repo".git
-            cd ..
-         else
-            git clone http://GITLAB_SERVER.localhost/project/"$repo".git
-         fi
-      done
-   elif [ $project = "PROJN" ]; then
-      listOfRepos="REPO1 REPO2 REPON"
-      for repo in $listOfRepos
-      do
-         # look for empty dir
-         if [ "$(ls -A $repo)" ]; then
-            echo "$repo is not Empty"
-            cd "$repo"
-            git pull http://GITLAB_SERVER.localhost/project/"$repo".git
-            cd ..
-         else
-            git clone http://GITLAB_SERVER.localhost/project/"$repo".git
-         fi
-      done
-   elif [ $project = "PROJ3" ]; then
-      listOfRepos="REPO1 REPO2 REPON"
-      for repo in $listOfRepos
-      do
-         # look for empty dir
-         if [ "$(ls -A $repo)" ]; then
-            echo "$repo is not Empty"
-            cd "$repo"
-            git pull http://GITLAB_SERVER.localhost/project/"$repo".git
-            cd ..
-         else
-            git clone http://GITLAB_SERVER.localhost/project/"$repo".git
-         fi
-      done
-   fi
+	echo "$project"
 
-   cd ../..
-   python jira_to_json.py $project
-   python git_to_json.py $project
-   python history_to_json.py $project
-   java -jar importerjson.jar $project
+	# Create backups of tracking files
+	for restoreFile in $restoreFiles
+	do
+		if [ -e "$project/$restoreFile" ]; then
+			cp "$project/$restoreFile" "$project/$restoreFile.bak"
+		fi
+	done
+
+	mkdir -p project-git-repos/$project
+	cd project-git-repos/$project
+	if [ $project = "PROJ2" ]; then
+		listOfRepos="REPO1 REPO2 REPON"
+		update_repositories "http://GITLAB_SERVER.localhost/project/%s.git" $listOfRepos
+	elif [ $project = "PROJ1" ]; then
+		listOfRepos="REPO1 REPO2 REPON"
+		update_repositories "http://GITLAB_SERVER.localhost/project/%s" $listOfRepos
+	elif [ $project = "PROJ4" ]; then
+		listOfRepos="REPO1 REPO2 REPON"
+		update_repositories "http://GITLAB_SERVER.localhost/project/%s.git" $listOfRepos
+	elif [ $project = "PROJN" ]; then
+		listOfRepos="REPO1 REPO2 REPON"
+		update_repositories "http://GITLAB_SERVER.localhost/project/%s.git" $listOfRepos
+	elif [ $project = "PROJ3" ]; then
+		listOfRepos="REPO1 REPO2 REPON"
+		update_repositories "http://GITLAB_SERVER.localhost/project/%s.git" $listOfRepos
+	fi
+
+	cd ../..
+	status_handler python jira_to_json.py $project
+	status_handler python git_to_json.py $project
+	status_handler python history_to_json.py $project
+	status_handler java -jar importerjson.jar $project
 done
