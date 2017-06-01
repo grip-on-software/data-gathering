abb9f26059d731c486286e91b1324d5032a584d0
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-06-01 13:53:42 +0200

    SVN: Rework difference parser
    - Split into different classes that handle the state of the parser,
      diff-wide or file-specific.
    - Fixe placement of change type: file-specific instead of for the entire
      diff, so must be added in change_path instead of the returned data.
    - Fixed counting number of files and lines

--- gatherer/svn/difference.py
+++ gatherer/svn/difference.py
@@ -8,6 +8,158 @@ import svn.exception
 from ..version_control import Change_Type
 from ..table import Table
 
+class Difference_State(object):
+    """
+    Tracking object for the current state of the difference parser through
+    token updates from the parser.
+    """
+
+    def __init__(self, diff):
+        self._diff = diff
+
+        self._file = None
+        self._insertions = 0
+        self._deletions = 0
+        self._number_of_files = 0
+        self._size = 0
+        self._head = True
+
+    def parse_line(self, line):
+        """
+        Parse an untokenized line that the parser retrieved.
+        """
+
+        if line.startswith(b'Index: '):
+            self._update_file()
+
+            self._number_of_files += 1
+            self._file = Difference_File(self._diff)
+            self._file.parse_index(line)
+        elif self._head:
+            self._head = self._file.parse_head(line)
+        else:
+            if self._file.parse_content(line):
+                self._size += len(line) - 1
+
+    def _update_file(self):
+        if self._file is not None:
+            self._file.update_table()
+            self._insertions += self._file.file_insertions
+            self._deletions += self._file.file_deletions
+
+    def get_data(self):
+        """
+        Retrieve the final information after processing the diff.
+        """
+
+        self._update_file()
+
+        number_of_lines = self._insertions + self._deletions
+        return {
+            # Statistics
+            'insertions': str(self._insertions),
+            'deletions': str(self._deletions),
+            'number_of_lines': str(number_of_lines),
+            'number_of_files': str(self._number_of_files),
+            'size': str(self._size)
+        }
+
+class Difference_File(object):
+    """
+    Tracking object for the current file-specific state of the difference
+    parser through token updates from the difference-wide state.
+    """
+
+    # Tokens that occurs in the header of the file that indicate the old/new
+    # file name after the token, as well as the old/new revision involved;
+    # if the file does not exist in that revision, then it is an addition or
+    # deletion depending on the token type as indicated here.
+    HEAD_TOKENS = {
+        b'---': Change_Type.ADDED,
+        b'+++': Change_Type.DELETED
+    }
+
+    def __init__(self, diff):
+        self._diff = diff
+        self._filename = None
+        self._change_type = None
+        self._file_insertions = 0
+        self._file_deletions = 0
+
+    @property
+    def file_insertions(self):
+        """
+        Retrieve the number of added lines in the diff for this file.
+        """
+
+        return self._file_insertions
+
+    @property
+    def file_deletions(self):
+        """
+        Retrieve the number of removed lines in the diff for this file.
+        """
+
+        return self._file_deletions
+
+    def parse_index(self, line):
+        """
+        Parse the index line containing the filename for this file.
+        """
+
+        self._filename = line[len(b'Index: '):]
+        self._change_type = Change_Type.MODIFIED
+
+    def parse_head(self, line):
+        """
+        Parse the header in the difference of this file.
+
+        Returns whether the header may still continue after this line.
+        """
+
+        for token, change_type in self.HEAD_TOKENS.items():
+            if line.startswith(token):
+                if line.endswith('(nonexistent)'):
+                    self._change_type = change_type
+
+                return True
+
+        if line.startswith(b'@@ '):
+            return False
+
+        return True
+
+    def parse_content(self, line):
+        """
+        Parse a line with difference content for this file.
+
+        Returns whether the line has an addition or deletion.
+        """
+
+        if line.startswith(b'+'):
+            self._file_insertions += 1
+        elif line.startswith(b'-'):
+            self._file_deletions += 1
+        else:
+            return False
+
+        return True
+
+    def update_table(self):
+        """
+        Update the table of file-based statistics.
+        """
+
+        if self._diff.version_id is not None:
+            self._diff.change_paths.append({
+                'repo_name': str(self._diff.repo.repo_name),
+                'version_id': str(self._diff.version_id),
+                'file': str(self._filename.decode('utf-8')),
+                'change_type': str(self._change_type.value),
+                'insertions': str(self._file_insertions),
+                'deletions': str(self._file_deletions)
+            })
+
 class Difference(object):
     """
     Parser for Subversion difference format.
@@ -63,6 +215,24 @@ class Difference(object):
         return self._parse_diff(diff_result)
 
     @property
+    def repo(self):
+        """
+        Retrieve the repository that this difference parser oprates on.
+        """
+
+        return self._repo
+
+    @property
+    def version_id(self):
+        """
+        Retrieve the single version for which the differences is retrieved,
+        in the case that the diff is made between its prior version and the
+        version at hand. In other cases, this property returns `None`.
+        """
+
+        return self._version_id
+
+    @property
     def change_paths(self):
         """
         Retrieve the table of file-based statistics retrieved from the diff.
@@ -71,70 +241,8 @@ class Difference(object):
         return self._change_paths
 
     def _parse_diff(self, diff_result):
-        insertions = 0
-        deletions = 0
-        filename = None
-        file_insertions = 0
-        file_deletions = 0
-        change_type = Change_Type.MODIFIED
-        number_of_lines = 0
-        number_of_files = 0
-        size = 0
-        head = True
+        state = Difference_State(self)
         for line in diff_result.splitlines():
-            if line.startswith(b'Index: '):
-                if filename is not None:
-                    self._parse_change_stats(filename, file_insertions,
-                                             file_deletions)
-                    insertions += file_insertions
-                    deletions += file_deletions
+            state.parse_line(line)
 
-                filename = line[len(b'Index: '):]
-                file_insertions = 0
-                file_deletions = 0
-                change_type = Change_Type.MODIFIED
-                head = True
-            elif head:
-                if line.startswith(b'---'):
-                    if line.endswith('(nonexistent)'):
-                        change_type = Change_Type.ADDED
-                elif line.startswith(b'+++'):
-                    if line.endswith('(nonexistent)'):
-                        change_type = Change_Type.DELETED
-                elif line.startswith(b'@@ '):
-                    head = False
-
-            if not head:
-                if line.startswith(b'+'):
-                    file_insertions += 1
-                    size += len(line) - 1
-                elif line.startswith(b'-'):
-                    file_deletions += 1
-                    size += len(line) - 1
-
-        if filename is not None:
-            self._parse_change_stats(filename, file_insertions, file_deletions)
-            insertions += file_insertions
-            deletions += file_deletions
-
-        number_of_lines = insertions + deletions
-
-        return {
-            # Statistics
-            'insertions': str(insertions),
-            'deletions': str(deletions),
-            'change_type': str(change_type.value),
-            'number_of_lines': str(number_of_lines),
-            'number_of_files': str(number_of_files),
-            'size': str(size)
-        }
-
-    def _parse_change_stats(self, filename, file_insertions, file_deletions):
-        if self._version_id is not None:
-            self._change_paths.append({
-                'repo_name': str(self._repo.repo_name),
-                'version_id': str(self._version_id),
-                'file': str(filename.decode('utf-8')),
-                'insertions': str(file_insertions),
-                'deletions': str(file_deletions)
-            })
+        return state.get_data()
