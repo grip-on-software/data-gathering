50575939a99aa2cd1db22f32fa137a8506cdd3ea
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-06-19 14:35:23 +0200

    Docker: Ensure path and environment are available
    - Keep the credentials environment variables around since they are
      required for correct public identity management
    - Full path to the bigboat script

--- docker-init.sh
+++ docker-init.sh
@@ -4,13 +4,15 @@ if [ ! -z "$CRON_PERIOD" ]; then
 	# Enable a cron job for scraping regularly
 	cat >/etc/periodic/$CRON_PERIOD/scrape <<SH
 #!/bin/sh
+export SOURCE_CREDENTIALS_ENV=${SOURCE_CREDENTIALS_ENV}
+export ${SOURCE_CREDENTIALS_ENV}="${!SOURCE_CREDENTIALS_ENV}"
 su agent -c '/home/agent/docker-scraper.sh ${JIRA_KEY}'
 SH
 	chmod +x /etc/periodic/$CRON_PERIOD/scrape
 
 	cat >/etc/periodic/15min/bigboat <<SH
 #!/bin/sh
-su agent -c 'python bigboat_to_json.py ${JIRA_KEY}'
+su agent -c 'python /home/agent/bigboat_to_json.py ${JIRA_KEY}'
 SH
 	chmod +x /etc/periodic/15min/bigboat
 
