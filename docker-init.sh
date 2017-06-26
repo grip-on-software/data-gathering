4d4029a8b6dd980d5a7878442ffcda714d9ee0e2
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-06-26 17:17:31 +0200

    Docker: Avoid undefined variables in periodic task
    Cron period could be set to a dash to disable, and source credentials
    may be unneeded.

--- docker-init.sh
+++ docker-init.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ ! -z "$CRON_PERIOD" ]; then
+if [ ! -z "$CRON_PERIOD" ] && [ "$CRON_PERIOD" != "-" ]; then
 	# Enable a cron job for scraping regularly
-	cat >/etc/periodic/$CRON_PERIOD/scrape <<SH
+	if [ ! -z "$SOURCE_CREDENTIALS_ENV" ] && [ "$SOURCE_CREDENTIALS_ENV" != "-" ]; then
+		cat >/etc/periodic/$CRON_PERIOD/scrape <<SH
 #!/bin/sh
 export SOURCE_CREDENTIALS_ENV=${SOURCE_CREDENTIALS_ENV}
 export ${SOURCE_CREDENTIALS_ENV}="${!SOURCE_CREDENTIALS_ENV}"
 su agent -c 'cd /home/agent && ./docker-scraper.sh ${JIRA_KEY}'
 SH
+	else
+		cat >/etc/periodic/$CRON_PERIOD/scrape <<SH
+#!/bin/sh
+su agent -c 'cd /home/agent && ./docker-scraper.sh ${JIRA_KEY}'
+SH
+	fi
+
 	chmod +x /etc/periodic/$CRON_PERIOD/scrape
 
 	cat >/etc/periodic/15min/bigboat <<SH
