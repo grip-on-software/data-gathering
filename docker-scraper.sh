f28e78efa094725661003e18eb4c0a81d9f00aed
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-06-27 09:51:43 +0200

    Agent: Remove old update files before scraping
    This ensures that the remote update trackers are always used.

--- docker-scraper.sh
+++ docker-scraper.sh
@@ -21,6 +21,11 @@ scripts="project_sources.py git_to_json.py"
 updateFiles=$(./list-files.sh update $scripts)
 exportFiles=$(./list-files.sh export $scripts)
 
+# Remove old update files so that the remote update trackers are always used
+for $updateFile in updateFiles; do
+	rm -f export/$project/$updateFile
+done
+
 python retrieve_metrics_repository.py $project --log $logLevel
 python retrieve_update_trackers.py $project --files $updateFiles --log $logLevel
 python project_sources.py $project --log $logLevel
