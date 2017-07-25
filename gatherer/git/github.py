c75f4fda11c7dc6dc601ba3353dcba7e86e940c3
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-07-25 17:42:41 +0200

    GitHub: Retrieve issue, pull request, comments

--- gatherer/git/github.py
+++ gatherer/git/github.py
@@ -20,15 +20,25 @@ class GitHub_Repository(Git_Repository, Review_System):
     Git repository hosted by GitHub.
     """
 
+    REVIEW_UPVOTE = 'APPROVED'
+    REVIEW_DOWNVOTE = 'CHANGES_REQUESTED'
+
     @property
     def review_tables(self):
         review_tables = super(GitHub_Repository, self).review_tables
-        review_fields = self.build_user_fields('reviewer')
+        author = self.build_user_fields('author')
+        assignee = self.build_user_fields('assignee')
+        reviewer = self.build_user_fields('reviewer')
         review_tables.update({
             "github_repo": Key_Table('github_repo', 'github_id'),
             "merge_request_review": Link_Table('merge_request_review',
                                                ('merge_request_id', 'reviewer'),
-                                               encrypted_fields=review_fields)
+                                               encrypted_fields=reviewer),
+            "github_issue": Key_Table('github_issue', 'issue_id',
+                                      encrypted_fields=author + assignee),
+            "github_issue_note": Link_Table('github_issue_note',
+                                            ('issue_id', 'note_id'),
+                                            encrypted_fields=author)
         })
         return review_tables
 
@@ -51,13 +61,13 @@ class GitHub_Repository(Git_Repository, Review_System):
 
         self.fill_repo_table(self._source.github_repo)
         for pull_request in self._source.github_repo.get_pulls():
-            newer = self.add_pull_request(pull_request)
+            newer, reviews = self.add_pull_request(pull_request)
             if newer:
                 for issue_comment in pull_request.get_issue_comments():
-                    self.add_issue_comment(issue_comment, pull_request.number)
+                    self.add_pull_comment(issue_comment, pull_request.number)
                 for review_comment in pull_request.get_review_comments():
                     self.add_commit_comment(review_comment, pull_request.number)
-                for review in pull_request.get_reviews():
+                for review in reviews:
                     self.add_review(review, pull_request.number)
 
         for issue in self._source.github_repo.get_issues():
@@ -100,14 +110,57 @@ class GitHub_Repository(Git_Repository, Review_System):
             "watch_count": str(repo.watchers_count)
         })
 
+    @staticmethod
+    def _get_username(part):
+        if part is None:
+            return str(0)
+
+        return parse_unicode(part.login)
+
+    def _format_issue(self, issue):
+        author_username = self._get_username(issue.user)
+        assignee_username = self._get_username(issue.assignee)
+        return {
+            'repo_name': str(self._repo_name),
+            'issue_id': str(issue.number),
+            'title': parse_unicode(issue.title),
+            'description': parse_unicode(issue.body),
+            'status': issue.state,
+            'author': author_username,
+            'author_username': author_username,
+            'assignee': assignee_username,
+            'assignee_username': assignee_username,
+            'created_at': format_date(issue.created_at),
+            'updated_at': format_date(issue.updated_at)
+        }
+
     def add_pull_request(self, pull_request):
         """
         Add a pull request described by its GitHub API response object to the
         merge requests table. Returns whether the pull request is updated more
-        recently than the update tracker date.
+        recently than the update tracker date and an iterable of the reviews
+        associated with the pull request.
         """
 
-        pass
+        if not self._is_newer(pull_request.updated_at):
+            return False, []
+
+        reviews = pull_request.get_reviews()
+        upvotes = len([1 for review in reviews if review.state == self.UPVOTE])
+        downvotes = len([
+            1 for review in reviews if review.state == self.DOWNVOTE
+        ])
+
+        request = self._format_issue(pull_request)
+        request.update({
+            'source_branch': pull_request.head.ref,
+            'target_branch': pull_request.base.ref,
+            'upvotes': str(upvotes),
+            'downvotes': str(downvotes)
+        })
+        self._tables["merge_request"].append(request)
+
+        return True, reviews
 
     def add_issue(self, issue):
         """
@@ -116,23 +169,79 @@ class GitHub_Repository(Git_Repository, Review_System):
         the update tracker date.
         """
 
-        pass
+        issue_row = self._format_issue(issue)
+        issue_row.update({
+            'labels': len(issue.labels),
+            'closed_at': format_date(issue.closed_at),
+            'closed_by': self._get_username(issue.closed_by)
+        })
+        self._tables["github_issue"].append(issue_row)
 
-    def add_issue_comment(self, note, issue_id):
+    def _format_note(self, comment):
+        author = self._get_username(comment.user)
+        return {
+            'repo_name': str(self._repo_name),
+            'note_id': comment.id,
+            'author': author,
+            'author_username': author,
+            'comment': parse_unicode(comment.body),
+            'created_at': format_date(comment.created_at),
+            'updated_at': format_date(comment.updated_at)
+        }
+
+    def add_issue_comment(self, comment, issue_id):
         """
-        Add an issue or normal pull request comment described by its GitHub API
-        response object to the repo merge request notes table.
+        Add an issue comment described by its GitHub API response object to the
+        issue notes table.
         """
 
-        pass
+        note = self._format_note(comment)
+        note['issue_id'] = issue_id
+        self._tables["github_issue_note"].append(note)
 
-    def add_commit_comment(self, note, request_id=None):
+    def add_pull_comment(self, comment, request_id):
+        """
+        Add a normal pull request comment described by its GitHub API response
+        object to the repo merge request notes table.
+        """
+
+        note = self._format_note(comment)
+        note.update({
+            'thread_id': str(0),
+            'parent_id': str(0),
+            'merge_request_id': request_id
+        })
+
+        self._tables["merge_request_note"].append(note)
+
+    def add_commit_comment(self, comment, request_id=0):
         """
         Add a commit comment or pull request review comment described by its
         GitHub API response object to the commit comments table.
         """
 
-        pass
+        note = self._format_note(comment)
+        if comment.position is None:
+            position = str(comment.original_position)
+        else:
+            position = str(comment.position)
+
+        note.update({
+            'thread_id': str(0),
+            'parent_id': str(0),
+            'merge_request_id': str(request_id),
+            'created_date': note['created_at'],
+            'updated_date': note['updated_at'],
+            'file': comment.path,
+            'line': position,
+            'end_line': position,
+            'line_type': str(0),
+            'commit_id': comment.commit_id
+        })
+        del note['created_at']
+        del note['updated_at']
+
+        self._tables["commit_comment"].append(note)
 
     def add_review(self, review, request_id):
         """
@@ -140,4 +249,18 @@ class GitHub_Repository(Git_Repository, Review_System):
         to the merge request reviews table.
         """
 
-        pass
+        reviewer = self._get_username(review.user)
+        if review.state == self.UPVOTE:
+            vote = 1
+        elif review.state == self.DOWNVOTE:
+            vote = -1
+        else:
+            vote = 0
+
+        self._tables["merge_request_review"].append({
+            'repo_name': str(self._repo_name),
+            'merge_request_id': request_id,
+            'reviewer': reviewer,
+            'reviewer_name': reviewer,
+            'vote': str(vote)
+        })
