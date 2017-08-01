612b350bafa61c5368e2670e7a97081432cc6e6b
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-08-01 11:54:29 +0200

    Jenkins: Improve data model, stability
    - Job: Cache last builds, use own data only if it is already available
      and otherwise build the URL to the last build ourselves. Add more
      properties for types of last builds, and expose the next build number.
    - Build: Retrieve build number from API if it was not known from the
      construction, e.g., for last builds. Make builds from the same job
      comparable using the ordering operators based on their number, e.g.,
      one can do `job.last_stable_build > job.last_failed_build`.
    - Objects are now comparable to each other as well as hashable.
    - Objects that end up not existing, e.g., last builds when there are no
      builds for the job, lose any data and become falsy, but they remain
      addressable where all their data items result in `None`.

--- gatherer/jenkins.py
+++ gatherer/jenkins.py
@@ -4,18 +4,40 @@ Module for accessing Jenkins build information and starting jobs.
 
 from builtins import object
 from abc import ABCMeta
+from collections import Mapping
 import json
 from future.utils import with_metaclass
 import requests
 from requests.auth import HTTPBasicAuth
 from requests.utils import quote
 
+class NoneMapping(Mapping): # pylint: disable=no-init
+    """
+    An empty mapping that returns `None` for all key lookups.
+    """
+
+    def __getitem__(self, key):
+        return None
+
+    def __iter__(self):
+        while False:
+            yield None
+
+    def __len__(self):
+        return 0
+
+    def __contains__(self, key):
+        return False
+
+    def __repr__(self):
+        return 'NoneMapping()'
+
 class Base(with_metaclass(ABCMeta, object)):
     """
     Base Jenkins object.
     """
 
-    def __init__(self, instance, base_url):
+    def __init__(self, instance, base_url, exists=True):
         self._instance = instance
 
         if not base_url.endswith('/'):
@@ -24,6 +46,7 @@ class Base(with_metaclass(ABCMeta, object)):
         self._base_url = base_url
         self._data = None
         self._has_data = False
+        self._exists = exists
 
     @property
     def base_url(self):
@@ -39,12 +62,28 @@ class Base(with_metaclass(ABCMeta, object)):
         Retrieve the raw data from the API.
         """
 
-        if not self._has_data:
+        if self._exists and not self._has_data:
             request = self.instance.session.get(self.base_url + 'api/json')
-            self._data = request.json()
-            self._has_data = True
+            if request.status_code == requests.codes['not_found']:
+                self._exists = False
+                self._data = NoneMapping()
+            else:
+                self._data = request.json()
+                self._has_data = True
 
         return self._data
+
+    @property
+    def has_data(self):
+        """
+        Retrieve a boolean indicating whether we fetched data for the object.
+        Returns `True` if and only if the current data is from the object's API
+        endpoint itself. This property returns `False` if the data is from
+        another API endpoint, e.g., a parent object, if the data has been
+        invalidated or if the dat has not been retrieved at all.
+        """
+
+        return self._has_data
 
     def invalidate(self):
         """
@@ -61,6 +100,29 @@ class Base(with_metaclass(ABCMeta, object)):
         """
 
         return self._instance
+
+    @property
+    def exists(self):
+        """
+        Retrieve whether this object exists on the Jenkins instance.
+        """
+
+        return self._exists
+
+    def __eq__(self, other):
+        if isinstance(other, Base):
+            return self.base_url == other.base_url
+
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __nonzero__(self):
+        return self.exists
+
+    def __hash__(self):
+        return hash((self.instance, self.base_url))
 
     def __repr__(self):
         return '{}({!r})'.format(self.__class__.__name__, self.base_url)
@@ -117,6 +179,15 @@ class Jenkins(Base):
 
         return [View(self, **view) for view in self.data['views']]
 
+    def __eq__(self, other):
+        if isinstance(other, Jenkins):
+            return self.base_url == other.base_url
+
+        return False
+
+    def __hash__(self):
+        return hash(self.base_url)
+
 class View(Base):
     """
     View on a Jenkins instance.
@@ -148,6 +219,12 @@ class View(Base):
 
         return [Job(self.instance, **job) for job in self.data['jobs']]
 
+    def __eq__(self, other):
+        if isinstance(other, View):
+            return self.instance == other.instance and self.name == other.name
+
+        return False
+
 class Job(Base):
     """
     Job on a Jenkins instance.
@@ -162,6 +239,7 @@ class Job(Base):
         super(Job, self).__init__(instance, url)
         self._name = name
         self._data = kwargs
+        self._last_builds = {}
 
     @property
     def name(self):
@@ -179,13 +257,26 @@ class Job(Base):
 
         return [Build(self, **build) for build in self.data['builds']]
 
+    def _make_last_build(self, name):
+        if name not in self._last_builds:
+            if self.has_data:
+                if self.data[name] is None:
+                    self._last_builds[name] = Build(self, exists=False)
+                else:
+                    self._last_builds[name] = Build(self, **self.data[name])
+            else:
+                url = '{}{}/'.format(self.base_url, name)
+                self._last_builds[name] = Build(self, url=url)
+
+        return self._last_builds[name]
+
     @property
     def last_build(self):
         """
-        Retrieve the last build, or `None`.
+        Retrieve the last build.
         """
 
-        return Build(self, **self.data['lastBuild'])
+        return self._make_last_build('lastBuild')
 
     @property
     def last_completed_build(self):
@@ -193,7 +284,55 @@ class Job(Base):
         Retrieve the last completed build.
         """
 
-        return Build(self.instance, **self.data['lastCompletedBuild'])
+        return self._make_last_build('lastCompletedBuild')
+
+    @property
+    def last_failed_build(self):
+        """
+        Retrieve the last failed build.
+        """
+
+        return self._make_last_build('lastFailedBuild')
+
+    @property
+    def last_stable_build(self):
+        """
+        Retrieve the last stable build.
+        """
+
+        return self._make_last_build('lastStableBuild')
+
+    @property
+    def last_successful_build(self):
+        """
+        Retrieve the last successful build.
+        """
+
+        return self._make_last_build('lastSuccessfulBuild')
+
+    @property
+    def last_unstable_build(self):
+        """
+        Retrieve the last unstable build.
+        """
+
+        return self._make_last_build('lastUnstableBuild')
+
+    @property
+    def last_unsuccessful_build(self):
+        """
+        Retrieve the last unsuccessful build.
+        """
+
+        return self._make_last_build('lastUnsuccessfulBuild')
+
+    @property
+    def next_build_number(self):
+        """
+        Retrieve the next build number.
+        """
+
+        return self.data['nextBuildNumber']
 
     def build(self, parameters=None, token=None):
         """
@@ -220,16 +359,20 @@ class Job(Base):
 
         return self.instance.session.post(url, params=params, data=data)
 
+    def __eq__(self, other):
+        if isinstance(other, Job):
+            return self.instance == other.instance and self.name == other.name
+
+        return False
+
 class Build(Base):
     """
     Build information of a certain job on a Jenkins instance.
     """
 
-    def __init__(self, job, number=None, url=None, **kwargs):
-        if number is None:
-            raise ValueError('Number is required')
+    def __init__(self, job, number=None, url=None, exists=True, **kwargs):
         if url is None:
-            url = '{}{}/'.format(job.base_url, number)
+            url = '{}{}/'.format(job.base_url, number, exists=exists)
         super(Build, self).__init__(job.instance, url)
         self._job = job
         self._number = number
@@ -249,4 +392,49 @@ class Build(Base):
         Retrieve the build number.
         """
 
+        if self._number is None:
+            self._number = self.data['number']
+
         return self._number
+
+    def _related(self, other):
+        if isinstance(other, Build):
+            return self.exists and other.exists and self.job == other.job
+
+        return False
+
+    def __eq__(self, other):
+        return self._related(other) and self.number == other.number
+
+    def __lt__(self, other):
+        if self._related(other):
+            return self.number < other.number
+
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self._related(other):
+            return self.number > other.number
+
+        return NotImplemented
+
+    def __lte__(self, other):
+        if self._related(other):
+            return self.number <= other.number
+
+        return NotImplemented
+
+    def __gte__(self, other):
+        if self._related(other):
+            return self.number >= other.number
+
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.job, self.number))
+
+    def __repr__(self):
+        if self.exists and self.has_data:
+            return 'Build({!r}, number={!r})'.format(self.job, self.number)
+
+        return super(Build, self).__repr__()
