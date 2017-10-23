847838410f234c351e6019a3b6b80a0a4c78d6a4
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-10-23 14:37:03 +0200

    Docker export: Improve upload and logging
    - Upload multiple files using the same SCP connection. This does not
      provide any I/O improvement but avoids the need to execute SCP
      multiple times, which causes less journal spam on upload server.
    - Add dry run mode to show what commands would be executed and what URLs
      requested
    - Handle runtime error when status response is incorrect, so that
      logging handler can use it
    - Keep return code of success/failure

--- export_files.py
+++ export_files.py
@@ -9,7 +9,9 @@ except ImportError:
     raise
 
 import argparse
+import logging
 import subprocess
+import sys
 import requests
 from gatherer.config import Configuration
 from gatherer.domain import Project
@@ -36,6 +38,8 @@ def parse_args():
                         help='update tracker files to consider')
     parser.add_argument('--export', nargs='+', default=[],
                         help='data files to consider for export')
+    parser.add_argument('--dry-run', dest='dry_run', action='store_true',
+                        default=False, help='Log actions rather than executing')
 
     Log_Setup.add_argument(parser)
     args = parser.parse_args()
@@ -43,26 +47,44 @@ def parse_args():
 
     return args
 
-def export_file(project, export_path, filename, key_path):
+def export_files(project, key_path, filenames, export_path, dry_run=False):
     """
-    Upload the export data file `filename` from the export directory for the
-    `project` to a remote SCP directory indicated by the `export_path`, using
-    the identity file in `key_path` for SSH public key authentication.
+    Upload the export data files `filenames` for the project `project` from
+    the export directory for the `project` to a remote SCP directory indicated
+    by the `export_path`, using the identity file in `key_path` for SSH public
+    key authentication.
+
+    If `dry_run` is `True`, then only log what command would be executed rather
+    than performing this action.
     """
 
-    subprocess.call([
-        'scp', '-i', key_path,
-        '{}/{}'.format(project.export_key, filename),
-        '{}/{}'.format(export_path, filename)
-    ])
+    args = ['scp', '-i', key_path] + [
+        '{}/{}'.format(project.export_key, filename) for filename in filenames
+    ] + [export_path + '/']
+    if dry_run:
+        logging.info('Dry run: Would execute %s', ' '.join(args))
+    else:
+        subprocess.call(args)
 
-def update_controller(host, project, cert):
+def update_controller(host, project, cert, dry_run=False):
     """
-    Indicate to the controller host that we are done with exporting the current
-    state of the gatherer.
+    Indicate to the controller API at domain `host` that we are done with
+    exporting the current state of the gatherer for the project `project`.
+
+    Use the certificate `cert` to verify the controller API.
+
+    If `dry_run` is `True`, then only log what URL would be requested rather
+    than performing this action.
+
+    If the requested URL returns a different status code than '202 Accepted',
+    then a `RuntimeError` is raised.
     """
 
     url = 'https://{}/auth/export.py?project={}'.format(host, project.jira_key)
+    if dry_run:
+        logging.info('Dry run: Would send a POST request to %s', url)
+        return
+
     request = requests.post(url, verify=cert)
 
     if request.status_code != requests.codes['accepted']:
@@ -79,10 +101,16 @@ def main():
     auth = args.agent + '@' + args.ssh
     path = '{}:~/{}'.format(auth, project.export_key)
 
-    for filename in args.export + args.update:
-        export_file(project, path, filename, args.path)
+    export_files(project, args.path, args.export + args.update, path,
+                 dry_run=args.dry_run)
 
-    update_controller(args.ssh, project, args.cert)
+    try:
+        update_controller(args.ssh, project, args.cert, dry_run=args.dry_run)
+    except RuntimeError:
+        logging.exception('Could not notify the export controller')
+        return 1
+
+    return 0
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
