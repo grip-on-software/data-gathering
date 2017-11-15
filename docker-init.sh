aabf4097dc3944e93b475f871c3bbb7795f654ab
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-11-15 15:06:15 +0100

    Agent: Do not load source credentials environment
    This is currently dead code because cron cannot reach the SSH_IDENTITY
    at this point anyway (and setting the environment variable pointing to
    it is not very spectacular either). The scraper should instead have
    environment credentials that already point to the correct identity file
    through configuration.

--- docker-init.sh
+++ docker-init.sh
@@ -4,16 +4,7 @@ if [ ! -z "$CRON_PERIOD" ] && [ "$CRON_PERIOD" != "-" ]; then
 	# Enable a cron job for scraping regularly
 	cat >/etc/periodic/$CRON_PERIOD/scrape <<'SH'
 #!/bin/sh
-set -o allexport
-source /home/agent/env
-source /home/agent/config/env
-set +o allexport
-
-if [ ! -z "$SOURCE_CREDENTIALS_ENV" ] && [ "$SOURCE_CREDENTIALS_ENV" != "-" ]; then
-	export SOURCE_CREDENTIALS_ENV=${SOURCE_CREDENTIALS_ENV}
-	export ${SOURCE_CREDENTIALS_ENV}="${!SOURCE_CREDENTIALS_ENV}"
-fi
-su agent -c 'cd /home/agent && ./docker-scraper.sh ${JIRA_KEY}'
+/home/agent/agent-env.sh 'cd /home/agent && ./docker-scraper.sh ${JIRA_KEY}'
 SH
 
 	chmod +x /etc/periodic/$CRON_PERIOD/scrape
