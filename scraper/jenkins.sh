902bf79e353de87ca48b21617909c492b51e83cc
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-01-17 09:34:31 +0100

    Scraper: Fix path to retrieve_importer.py
    This script was moved to the scraper directory.

--- scraper/jenkins.sh
+++ scraper/jenkins.sh
@@ -268,7 +268,7 @@ fi
 # Retrieve Java importer
 rm -f data_vcsdev_to_dev.json
 log_info "Retrieving importer"
-python retrieve_importer.py --jenkins --base $IMPORTER_BASE --log $logLevel
+python scraper/retrieve_importer.py --jenkins --base $IMPORTER_BASE --log $logLevel
 if [ -z "$gathererScripts" ] && [ "$importerTasks" != "skip" ]; then
 	files=$(import_handler --files $importerTasks)
 fi
