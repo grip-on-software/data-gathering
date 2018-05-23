4d7027c4dfef614c644d5b4c13517e8e8fc1a933
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-05-23 13:03:01 +0200

    Agent: Actually export log file

--- scraper/agent/scrape.sh
+++ scraper/agent/scrape.sh
@@ -5,4 +5,4 @@ chown -R agent:agent /home/agent/.ssh
 chmod -R 600 /home/agent/.ssh
 chmod 700 /home/agent/.ssh
 /home/agent/scraper/agent/env.sh 'cd /home/agent && /home/agent/scraper/agent/run.sh ${JIRA_KEY} "${PREFLIGHT_ARGS}" 2>&1 | tee /home/agent/export/scrape.log'
-/home/agent/scraper/agent/env.sh 'cd /home/agent && python /home/agent/scraper/export_files.py --other /home/agent/export/scrape.log --log INFO'
+/home/agent/scraper/agent/env.sh 'cd /home/agent && python /home/agent/scraper/export_files.py ${JIRA_KEY} --other /home/agent/export/scrape.log --log INFO'
