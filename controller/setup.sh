1241e34ba6097fe4d381d01a2f4f17049522d10e
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-09-09 12:56:28 +0200

    Controller: Add virtualenv.sh symlink for services

--- controller/setup.sh
+++ controller/setup.sh
@@ -3,6 +3,7 @@
 cp controller/{pyro-ns,gros-controller,gros-gatherer,gros-exporter}.service /etc/systemd/system/
 ln -s controller/export.sh /usr/local/bin/controller-export.sh
 ln -s controller/upload.sh /usr/local/bin/upload.sh
+ln -s controller/virtualenv.sh /usr/local/bin/virtualenv.sh
 ln -s controller/controller_daemon.py /usr/local/bin/controller-daemon.py
 ln -s controller/exporter_daemon.py /usr/local/bin/exporter-daemon.py
 ln -s controller/gatherer_daemon.py /usr/local/bin/gatherer-daemon.py
