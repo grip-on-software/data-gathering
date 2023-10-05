a062698b98ab42f2180844a86bffd36aa1933507
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2023-10-05 12:56:19 +0200

    Consider Python 3.10 as maximum version for now
    Some outdated requirements that use lxml at a fixed version mean the
    latest Python headers are incompatible (moved in Python 3.11), so unless
    we work around the old versions like we do with python-gitlab, we are
    stuck with that version. Further testing would be necessary to ensure
    this is possible, but for now use Python 3.10.

--- setup.py
+++ setup.py
@@ -83,9 +83,7 @@ build automation servers (Jenkins) and reservation systems (Topdesk).''',
               'Programming Language :: Python :: 3.7',
               'Programming Language :: Python :: 3.8',
               'Programming Language :: Python :: 3.9',
-              'Programming Language :: Python :: 3.10',
-              'Programming Language :: Python :: 3.11',
-              'Programming Language :: Python :: 3.12'
+              'Programming Language :: Python :: 3.10'
           ],
           keywords='gros software development process data gathering')
 
