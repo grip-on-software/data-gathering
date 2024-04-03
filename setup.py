5531a74b22d2c4944e94f37b6983dbe640e99e39
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2024-04-03 13:31:51 +0200

    Mention 0.0.3 as last version for Python 3.6, bump development version

--- setup.py
+++ setup.py
@@ -79,7 +79,6 @@ build automation servers (Jenkins) and reservation systems (Topdesk).''',
               'Topic :: System :: Monitoring',
               'License :: OSI Approved :: Apache Software License',
               'Programming Language :: Python :: 3',
-              'Programming Language :: Python :: 3.6',
               'Programming Language :: Python :: 3.7',
               'Programming Language :: Python :: 3.8'
           ],
